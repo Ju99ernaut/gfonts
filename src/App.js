04af56d38647b32eb7fa097bd3a997da40e762bf
Ju99ernaut <brendonzee@gmail.com>
2021-02-20 16:57:45 +0200

    Add navbar in mobile view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import WebFont from 'webfontloader';
 import $ from 'cash-dom';
+import Sidebar from "react-sidebar";
 import Settings from './Settings';
 import Fonts from './Fonts';
 import Modal from './Modal';
+import Navbar from './Settings/Navbar';
 import 'font-awesome/css/font-awesome.min.css';
+
+const mql = window.matchMedia(`(min-width: 600px)`);
 
 class App extends React.Component {
   constructor(props) {
@@ -20,8 +24,23 @@ class App extends React.Component {
       data: [],
       font: {family: '', variants: [], subsets: []},
       stars: stars ? JSON.parse(stars) : [],
-      groupSize: 900
+      groupSize: 900,
+      sidebarDocked: mql.matches,
+      sidebarOpen: false
     };
+    mql.addListener(this.mediaQueryChanged);
+  }
+
+  componentWillUnmount() {
+    mql.removeListener(this.mediaQueryChanged);
+  }
+
+  onSetSidebarOpen = (open) => {
+    this.setState({ sidebarOpen: open });
+  }
+
+  mediaQueryChanged = () => {
+    this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
   }
 
   getSetting = (s) => {
@@ -66,9 +85,17 @@ class App extends React.Component {
 
     return (
       <div id="app">
-        <Settings onChange={this.getSetting} />
-        <Fonts setModal={this.setModal} settings={settings} stars={this.state.stars} onChange={this.setStars} />
-        <Modal font={this.state.font} stars={this.state.stars} onChange={this.setStars} />
+        <Sidebar
+          sidebar={<Settings onChange={this.getSetting} />}
+          open={this.state.sidebarOpen}
+          docked={this.state.sidebarDocked}
+          onSetOpen={this.onSetSidebarOpen}
+          styles={{ content: { overflowY: 'hidden' } }}
+        >
+          <Navbar onClick={() => this.onSetSidebarOpen(true)}/>
+          <Fonts setModal={this.setModal} settings={settings} stars={this.state.stars} onChange={this.setStars} />
+          <Modal font={this.state.font} stars={this.state.stars} onChange={this.setStars} />
+        </Sidebar>
       </div>
     );
   }
