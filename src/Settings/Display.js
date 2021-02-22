e06c969bb10941ebef163cddbbc8ec74e0be364b
Ju99ernaut <brendonzee@gmail.com>
2021-02-22 10:31:08 +0200

    Store theme in local storage

--- src/Settings/Display.js
+++ src/Settings/Display.js
@@ -1,10 +1,21 @@
 // jshint ignore: start
 
 import $ from 'cash-dom';
+import React from 'react';
 
-const Display = ({ onClick }) => {
-  const changeDisplay = (event) => {
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+    const theme = localStorage.getItem("theme");
+    this.state = {
+      dark: theme === "dark" ? true : false,
+    };
+    if(theme === "dark") $("body").addClass("invert");
+  }
+
+  changeDisplay = (event) => {
     const btn = $(event.target);
+    const { onClick } = this.props;
 
     if (!btn.hasClass("active")) {
       const value = event.target.getAttribute('data-value').toLowerCase();
@@ -19,38 +30,44 @@ const Display = ({ onClick }) => {
     }
   }
 
-  const changeSize = (event) => {
+  changeSize = (event) => {
     const size = $(event.target).val() + 'px';
     $('.fonts').css('font-size', size);
     $('.size').text(size);
   }
 
-  const invert = (event) => {
-    $(event.target).toggleClass("active");
+  invert = (event) => {
+    const dark = !$(event.target).hasClass("active");
+    localStorage.setItem("theme", dark ? "dark" : "light");
+    this.setState({ dark });
     $("body").toggleClass("invert");
   }
 
-  return (
-    <div className="display">
-      <div>
-        <h2>Display</h2>
-        <span onClick={changeDisplay} data-value="grid" className="btn btn-grid active" title="Switch to Grid">
-          <i className="fa fa-th"></i>
-        </span>
-        <span onClick={changeDisplay} data-value="row" className="btn btn-row" title="Switch to List">
-          <i className="fa fa-align-justify"></i>
-        </span>
-        <span onClick={invert} className="btn" title="Invert Colors">
-          <i className="fa fa-adjust"></i>
-        </span>
+  render() {
+    const { changeDisplay, invert, changeSize } = this;
+
+    return (
+      <div className="display">
+        <div>
+          <h2>Display</h2>
+          <span onClick={changeDisplay} data-value="grid" className="btn btn-grid active" title="Switch to Grid">
+            <i className="fa fa-th"></i>
+          </span>
+          <span onClick={changeDisplay} data-value="row" className="btn btn-row" title="Switch to List">
+            <i className="fa fa-align-justify"></i>
+          </span>
+          <span onClick={invert} className={this.state.dark ? "btn active" : "btn"} title="Invert Colors">
+            <i className="fa fa-adjust"></i>
+          </span>
+        </div>
+        <div>
+          <span className="size">30px</span>
+          <h2>Preview Size</h2>
+          <input type="range" defaultValue="30" min="10" max="80" onChange={changeSize} />
+        </div>
       </div>
-      <div>
-        <span className="size">30px</span>
-        <h2>Preview Size</h2>
-        <input type="range" defaultValue="30" min="10" max="80" onChange={changeSize} />
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Display;
