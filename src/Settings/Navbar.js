036e01003c10bcd87e1d8b8ffa0abd904d3f4614
Ju99ernaut <brendonzee@gmail.com>
2021-02-22 10:48:38 +0200

    Update Navbar.js

--- src/Settings/Navbar.js
+++ src/Settings/Navbar.js
@@ -3,10 +3,23 @@
 const Navbar = ({ onClick }) => {
   return (
     <div className="nav">
-      <button className="sidebar-open" onClick={onClick}>
-        <i className="fa fa-bars"></i>
-      </button>
-      <a href="./"><img alt="logo" width="20px" src="favicon.svg"/></a>
+      <span>
+        <button className="sidebar-open" onClick={onClick}>
+          <i className="fa fa-bars"></i>
+        </button>
+        <a href="./"><img alt="logo" width="20px" src="favicon.svg"/></a>
+      </span>
+      <span>
+        <a href="http://github.com/Ju99ernaut/gfonts/">
+          <i className="fa fa-github"></i>
+        </a>
+        <a href="https://twitter.com/Ju99ern4ut">
+          <i className="fa fa-twitter"></i>
+        </a>
+        <a href="https://www.facebook.com/sharer/sharer.php?u=https://gfonts.blocomposer.com">
+          <i className="fa fa-facebook"></i>
+        </a>
+      </span>
     </div>
   );
 };
